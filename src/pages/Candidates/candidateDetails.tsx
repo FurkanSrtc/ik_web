001c5a97858af3b39aadd785ad471adfe178ad12
Furkan Sertçe <furkansertc@gmail.com>
2022-05-01 16:43:08 +0300

    Updated candidate styles

--- src/pages/Candidates/candidateDetails.tsx
+++ src/pages/Candidates/candidateDetails.tsx
@@ -23,8 +23,8 @@ export default function CandidateDetails() {
         <h1 className='second-color'>Candidate Details</h1>
       </div>
       <div className='user-card'>
-        <div >
-          <img style={{ borderRadius: "50%" }} width={200} src={require("../../assets/images/user.png")} alt="candidate" />
+        <div>
+          <img style={{ borderRadius: "50%", display:"block", margin:"0 auto" }} width={200} src={require("../../assets/images/user.png")} alt="candidate" />
           <br/>
           <h2 style={{textAlign:"center"}}>{candidate?.name}</h2>
         
@@ -38,14 +38,11 @@ export default function CandidateDetails() {
           </h3>
 
         </div>
-
           <div style={{position:"absolute", textAlign:"center", left:0, right:0,padding:5 }}>
-          
           <a
             href={`https://maps.google.com/maps?q=${candidate?.address.geo.lat},${candidate?.address.geo.lng}&hl=es;z=14&amp;output=embed`}
             style={{ color: "#0000FF", padding: 5 }}
-            target="_blank" rel="noreferrer"
-          >
+            target="_blank" rel="noreferrer">
             <BiMap size={50} className="icon" />
           </a>
           <a
@@ -64,13 +61,8 @@ export default function CandidateDetails() {
           </a>
           </div>
           </div>
-
-        
-
       </div>
       <iframe title='map' src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2872.4091682232647!2d$${candidate?.address.geo.lat}!3d${candidate?.address.geo.lng}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa7c5ad2711135d1d!2zNDPCsDU3JzAzLjIiUyAzNMKwMjcnNDIuNSJX!5e0!3m2!1str!2str!4v1651411776867!5m2!1str!2str`} width="100%" height="450" style={{border:0}} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-
-
     </>
   );
 }
